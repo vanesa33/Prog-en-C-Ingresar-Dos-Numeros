b0fb1667fd4c694641c0d396b907f1964cd8037a
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-10 17:13:56 -0300

    Ingresar dos números
    El Usuario Ingresa Dos Números y Realizamos una Operación, C# consola

---
+++ IngresarNumeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IngresarDosNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM1, NUM2;
+            double RESUL;
+            string linea;
+            Console.Write("NÚMERO 1 :"); linea = Console.ReadLine();
+            NUM1 = int.Parse(linea);
+            Console.Write("NÚMERO 2 :"); linea = Console.ReadLine();
+            NUM2 = int.Parse(linea);
+            RESUL = (NUM1 + NUM2) * (NUM1 - NUM2);
+            Console.WriteLine();
+            Console.WriteLine("El resultado es : " + RESUL);
+            Console.ReadLine();
+        }
+    }
+}
